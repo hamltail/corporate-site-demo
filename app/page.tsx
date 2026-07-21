208ebe35f4eb2f07141e7106e388c4b96301b747
h-waji <goo.hiro.w@gmail.com>
2026-07-21 15:20:24 +0900

    Newsセクションを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import News from "@/components/News";
 import Projects from "@/components/Projects";
 import Service from "@/components/Service";
 import Team from "@/components/Team";
@@ -12,6 +13,7 @@ export default function Home() {
       <Service />
       <Projects />
       <Team />
+      <News />
     </main>
   );
 }
