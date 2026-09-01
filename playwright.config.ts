1713ee21eddd45e12b0889a35407e6f3aa838f9c
h-waji <goo.hiro.w@gmail.com>
2026-09-01 13:59:25 +0900

    i18n対応に合わせてE2Eテストを拡張

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
 
   use: {
     baseURL: "http://localhost:3000",
-    locale: "ja-JP",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
@@ -17,8 +16,20 @@ export default defineConfig({
 
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "chromium-ja",
+      testIgnore: /i18n\.spec\.ts/,
+      use: {
+        ...devices["Desktop Chrome"],
+        locale: "ja-JP",
+      },
+    },
+    {
+      name: "chromium-en",
+      testMatch: /i18n\.spec\.ts/,
+      use: {
+        ...devices["Desktop Chrome"],
+        locale: "en-US",
+      },
     },
   ],
 
